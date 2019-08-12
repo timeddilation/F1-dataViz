647415063c06277ad959bb183550296de2ae7b83
Tyler Neumann <neumanntylers@gmail.com>
2019-08-11 23:15:05 -0400

    Clean up and code comments

--- lapTimesDensity.R
+++ lapTimesDensity.R
@@ -1,23 +1,23 @@
 ### animate that shit ###
+# set the circuit to evaluate
 raceCounts <- races[raceId %in% unique(lapTimes[, raceId]), .(races = .N), by = circuitId][order(races)]
 mostRacesCircuitId <- as.numeric(raceCounts[nrow(raceCounts), circuitId])
 mostRacesCircuitId <- 18 #static, doing brazil GP for test
-
+# only pull races that have lapTimes data
 racesWithTimes <- unique(lapTimes[, raceId])
 racesForCircuit <- races[circuitId == mostRacesCircuitId][raceId %in% racesWithTimes][order(year)]
+# pull all lap times for races, and create a "seconds" column for the lap time
 allCircuitLapTimes <- lapTimes[raceId %in% racesForCircuit[, raceId]]
 allCircuitLapTimes[, seconds := milliseconds / 1000]
-
+# limit lap times to those under 3 minutes, removes pit stop laps and exceedingly slow laps that might throw off intended resutls
 anaimateLapTimesData <- allCircuitLapTimes[seconds <= 180]
 anaimateLapTimesData <- merge(anaimateLapTimesData, races[, .(raceId, name, year)])
-
+# retrieve race results for all races on circuit
 racesResults <- results[raceId %in% unique(anaimateLapTimesData[, raceId])]
 racesResults <- merge(racesResults, races[, .(raceId, year)])
 racesResults[, fastestLapTimeSeconds := convertLapTimeStringToSeconds(fastestLapTime)]
 racesResults[, fastestLapSpeed := as.numeric(fastestLapSpeed)]
-
-# racesResults <- racesResults[!is.na(fastestLapTimeSeconds), .(fastestLapSpeed = max(fastestLapSpeed), fastestLapTime = min(fastestLapTimeSeconds)), by = year]
-
+# group race results for fastest lap time and fastest lap speed
 racesResults <- racesResults[, .(fastestLapSpeed = max(fastestLapSpeed, na.rm = TRUE), fastestLapTime = min(fastestLapTimeSeconds, na.rm = TRUE)), by = year]
 racesResults[, fastestLapSpeed := as.character(fastestLapSpeed)][, fastestLapTime := as.character(fastestLapTime)]
 racesResults[fastestLapSpeed == "-Inf", fastestLapSpeed := "No Data"][fastestLapTime == "Inf", fastestLapTime := "No Data"]
@@ -27,12 +27,10 @@ racesResults[, raceToolTip := paste("<span style='font-size:18; color:black'>",
                                     "<br>**Fastest Lap Speed:** ", as.character(fastestLapSpeed),
                                     "</span>",
                                     sep = "")]
-# anaimateLapTimesData <- merge(anaimateLapTimesData, racesResults[, .(year, raceToolTip)], by.x = c("year"), by.y = c("year"), all.x = TRUE)
-
-# circuitMedianFastestSpeed <- boxplot.stats(racesResults[!is.na(fastestLapSpeed), fastestLapSpeed])$stats[3]
-
+# boxplot stats for static reference lines
 circuitBoxPlotStats <- boxplot.stats(anaimateLapTimesData[, seconds])
 vlines <- as.data.table(circuitBoxPlotStats$stats[1:5])
+# circuitMedianFastestSpeed <- circuitBoxPlotStats[3]
 
 grandPrixName <- anaimateLapTimesData[1, name]
 circuitImg <- getTrackImage(mostRacesCircuitId)
@@ -61,7 +59,6 @@ ggani <- ggplot(anaimateLapTimesData, aes(x = seconds)) +
   # watermark, track image, and fastest lap/speed
   annotation_custom(watermark, xmin = 150, xmax = 180, ymin = 0.28, ymax = 0.32) +
   annotation_custom(circuitImg, xmin = 150, xmax = 180, ymin = 0.18, ymax = 0.28) +
-  # geom_text(data = racesResults[, .(year, raceToolTip)], aes(x = 165, y = 0.16, label = raceToolTip)) +
   geom_rich_text(data = racesResults[, .(year, raceToolTip)], aes(x = 165, y = 0.16, label = raceToolTip),
                  fill = NA, label.color = NA) +
   # gganimate stuff
@@ -69,13 +66,18 @@ ggani <- ggplot(anaimateLapTimesData, aes(x = seconds)) +
   enter_fade() + 
   exit_shrink() +
   ease_aes('sine-in-out')
-ggani
+# ggani
+
+### You may need to run this (it will fail) to set the proper dimension of the gif
+### After running (and failing to run), run the code after this to actually create the gif with the correct dimensions
 # animate(plot = ggani, nframes = 110, end_pause = 10, ref_frame = 1, fps = 10, duration = 10, detail = 1,
 #         options(gganimate.dev_args = list(width = 960, height = 540)))
+
+# setup some params to generate the final gif
 gps <- length(unique(anaimateLapTimesData[, raceId]))
 framesPerGp <- 16
 totalFrames <- (gps * framesPerGp) + 15
-rm(gps, framesPerGp, allCircuitLapTimes, anaimateLapTimesData, vlines, grandPrixName)
+rm(gps, framesPerGp, allCircuitLapTimes, anaimateLapTimesData, vlines, grandPrixName, racesResults)
 
 animate(ggani, start_pause = 5, end_pause = 10, nframes = totalFrames, detail = 4)
 rm(totalFrames, ggani, circuitImg)
