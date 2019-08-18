13ea40f72117b5c7025569431b589fca4fbd799e
Tyler Neumann <neumanntylers@gmail.com>
2019-08-17 21:06:48 -0400

    Clarifications on data issue and approach to solve.

--- data_issue.R
+++ data_issue.R
@@ -1,10 +1,17 @@
-### Documented issue with lap time data, this greatl effects all stats with lap times
-### It seems that some lap times in the data remove the pit stop time, and other do not
-### Delow is a demonstration, where in the 2 races viewed below, there are inconsistencies
+### Documented issue with lap time data, this greatly effects all stats with lap times
+### It seems that some lap times in the data remove the pit stop time, and others do not
+### Below is a demonstration, where in the 2 races viewed, there are inconsistencies
 ### In the first case, subtracting the stop time from the lap time results in a "normal" lap time
-### In the second case, subtracting the stop time from the lap time results in "impossible" times
+### In the second case, subtracting the stop time from the lap time results in "impossibly fast" times
 ### Until this is fixed, all stats will be skewed.
 ### It currently appears older races do not account for stops, and new races do account for stops
+### If I new when this change in the database begins, I could write a tool to fix the data, however...
+### The fix assumes that simply subtracting the pit duration from the lap time is an "accurate" lap time
+library(data.table)
+
+lapTimes <<- fread("data/lap_times.csv")
+names(lapTimes) <- c("raceId","driverId","lap","position","time","milliseconds")
+lapTimes[, seconds := milliseconds / 1000]
 
 pitStops <<- fread("data/pit_stops.csv")
 names(pitStops) <- c("raceId","driverId","stop","lap","time","duration","milliseconds")
@@ -17,5 +24,5 @@ lapTimesWithoutPits <- merge(lapTimes,
 lapTimesWithoutPits[, stopDuration := as.double(stopDuration)][is.na(stopDuration), stopDuration := 0]
 lapTimesWithoutPits[, secondsLessStop := seconds - stopDuration]
 
-View(lapTimesWithoutPits[raceId == 841])
-View(lapTimesWithoutPits[raceId == 1021])
+View(lapTimesWithoutPits[raceId == 841]) # Australia 2011 - subtracting stop time looks correct
+View(lapTimesWithoutPits[raceId == 1021]) # Hungary 2019 - subtracting stop time results in impossibly fast times
